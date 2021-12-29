02f4a3377821b95cb4dedf1d7ec0523031980897
Johnna Murphy <johnnamurphy16@gmail.com>
2021-12-28 17:29:36 -0700

    Problem 7 Solution

--- problems/07-my-some.js
+++ problems/07-my-some.js
@@ -23,8 +23,16 @@ let result3 = mySome(['soup', 'noodles', 'bike', 'ship'], function(ele) {
 console.log(result3);   // true
 *******************************************************************************/
 
-let mySome = function() {
+let mySome = function(arr, cb) {
 
+    for (let i = 0; i < arr.length; i++) {
+        let element = arr[i];
+
+        if (cb(element, i)) {
+            return true;
+        }
+    }
+    return false;
 };
 
 
